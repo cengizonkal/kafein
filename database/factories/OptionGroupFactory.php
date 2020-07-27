7219b7c7e1e4adb9cba15ece390f0d70d9966052
cengiz <conkal@ciu.edu.tr>
2020-07-27 08:58:32 +0300

    optionGroups

---
+++ database/factories/OptionGroupFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\OptionGroup;
+use Faker\Generator as Faker;
+
+$factory->define(OptionGroup::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_07_27_055030_create_option_groups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOptionGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('option_groups', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('is_multiple')->default(false);
+            $table->string('title');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('option_groups');
+    }
+}
