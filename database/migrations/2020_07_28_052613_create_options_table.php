6246c807e93dc86af50858d5fa1eb61daceaaf27
cengiz <conkal@ciu.edu.tr>
2020-07-28 08:33:35 +0300

    options

--- database/migrations/2020_07_28_052613_create_options_table.php
+++ database/migrations/2020_07_28_052613_create_options_table.php
@@ -15,6 +15,13 @@ class CreateOptionsTable extends Migration
     {
         Schema::create('options', function (Blueprint $table) {
             $table->id();
+            $table->decimal('price')->default(0);
+            $table->string('name');
+            $table->unsignedBigInteger('option_group_id');
+            $table->foreign('option_group_id')
+                ->references('id')
+                ->on('option_groups')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
