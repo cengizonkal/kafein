40f7cd9f088e3f0aaeacee98511be2a70cf26aaf
cengiz <conkal@ciu.edu.tr>
2020-07-28 08:34:57 +0300

    options

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,7 +11,6 @@ use Illuminate\Database\Eloquent\Model;
  * @property mixed service_table_id
  * @property mixed item_id
  * @property mixed order_status_id
- * @property array options
  * @property mixed comment
  * @property mixed created_at
  * @property mixed updated_at
@@ -19,7 +18,5 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Order extends Model
 {
-    protected $casts = [
-        'options' => 'array',
-    ];
+
 }
--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -10,7 +10,7 @@ $factory->define(Order::class, function (Faker $faker) {
         'service_table_id' => \App\Models\ServiceTable::all()->random(1)->first()->id,
         'item_id' => \App\Models\Item::all()->random(1)->first()->id,
         'order_status_id' => \App\Models\OrderStatus::all()->random(1)->first()->id,
-        'options' => ['onion' => 'no', 'sugar' => 'yes'],
+
         'comment' => $faker->text(50),
     ];
 });
