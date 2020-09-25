a36f3bc88c2901fc7b43304f4910d0301aa2af90
cengiz önkal <onkal.cengiz@gmail.com>
2020-09-25 21:57:36 +0300

    OptionGroup.php

---
+++ app/Http/Controllers/Admin/OptionGroupController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Item;
+use App\Models\OptionGroup;
+use Illuminate\Http\Request;
+
+class OptionGroupController extends Controller
+{
+
+    public function index(Item $item)
+    {
+        return view('admin/option_group_index');
+    }
+
+
+    public function create(Item $item)
+    {
+        //
+    }
+
+
+    public function store(Request $request, Item $item)
+    {
+        //
+    }
+
+
+    public function show(Item $item, OptionGroup $optionGroup)
+    {
+        //
+    }
+
+
+    public function edit(Item $item, OptionGroup $optionGroup)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Item $item, OptionGroup $optionGroup)
+    {
+        //
+    }
+
+
+    public function destroy(Item $item, OptionGroup $optionGroup)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,10 @@ Route::delete('admin/items/{item}', 'Admin\ItemController@destroy');
 
 Route::get('admin/items/{item}/option-groups', 'Admin\Item\OptionGroupController@index');
 Route::get('admin/items/{item}/option-groups/create', 'Admin\Item\OptionGroupController@create');
+Route::post('admin/items/{item}/option-groups', 'Admin\Item\OptionGroupController@store');
+Route::get('admin/items/{item}/option-groups/edit', 'Admin\Item\OptionGroupController@edit');
+Route::patch('admin/items/{item}/option-groups/{optionGroup}', 'Admin\Item\OptionGroupController@update');
+Route::delete('admin/items/{item}/option-groups/{optionGroup}', 'Admin\Item\OptionGroupController@destroy');
 
 
 
